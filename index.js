3b3225d9dccfa7decbd68ec26600a008f18aa0cd
Chance Harmon <henrikillsit@gmail.com>
2019-08-20 22:32:05 -0700

    think it works

--- index.js
+++ index.js
@@ -15,3 +15,37 @@ class LinkedList {
 
   }
 }
+
+insertBefore(data) {
+  this.head = new Node(data, this.head);
+}
+
+valueMinusK(index) {
+  let current = this.head;
+  let count = 0;
+  let k = current.null;
+
+  while (current) {
+    if (count === index) {
+      console.log(current.data);
+    }
+    count++;
+    current
+  }
+  for (let i = 0; i < count - k; i++) {
+    return current.data
+  }
+}
+
+
+
+
+
+
+
+
+
+newNode.insertBefore(100);
+newNode.insertBefore(200);
+newNode.insertBefore(300);
+newNode.valueMinusK(1);
